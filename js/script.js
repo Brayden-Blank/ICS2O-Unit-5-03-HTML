a2eeb77c8b4963d3337dd80d49945a61c1c61f38
Brayden Blank <99354754+Brayden-Blank@users.noreply.github.com>
2022-04-28 17:19:29 +0000

    added radio buttons and started js

--- js/script.js
+++ js/script.js
@@ -10,36 +10,9 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-Unit-5-01-HTML/sw.js", {
-    scope: "/ICS2O-Unit-5-01-HTML/",
+  navigator.serviceWorker.register("/ICS2O-Unit-5-03-HTML/sw.js", {
+    scope: "/ICS2O-Unit-5-03-HTML/",
   });
 }
 
-const randomNumber = Math.floor(Math.random() * 6) + 1;
-
-/**
- * This function updates the slider value.
- */
-function updateSliderValue(valueFromSlider) {
-  document.getElementById("slider-value").innerHTML = valueFromSlider;
-}
-
-/**
- * This function compares slider with random number.
- */
-function guessClicked() {
-  const sliderValue = slider.value;
-
-  // process
-  if (sliderValue == randomNumber) {
-    document.getElementById("guess-result").innerHTML =
-      "Answer is " + randomNumber + "!" + " Congratulations!";
-    // console.log("Congratulations!")
-  }
-  //  block of code to be executed if condition1 is true
-  if (sliderValue != randomNumber) {
-    document.getElementById("guess-result").innerHTML =
-      "Answer is " + randomNumber + "!" + " Try again!";
-    // console.log("Try again!")
-  }
-}
+// This function 
